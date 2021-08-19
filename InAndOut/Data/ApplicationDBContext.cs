521d8a4d2757a97ba3d5fc6969c6743c329e3124
Jaspinder Singh <jaspinder@comarm.com>
2021-08-19 15:54:42 -0400

    Add project files.

---
+++ InAndOut/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
